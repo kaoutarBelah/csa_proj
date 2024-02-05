a1297eb97b7272d4efb1b83b03bc75f0ed2ad274
kaoutarBelah <kawtar2002azrou@gmail.com>
2024-02-05 13:56:53 +0100

    hh

--- src/component/ListEnseignantsComponent.tsx
+++ src/component/ListEnseignantsComponent.tsx
@@ -54,47 +54,45 @@ const ListEnseignantComponent: React.FC = () => {
 
     return (
         <div className="container">
-        <h2 className="text-center">List Enseignants</h2>
-    {/* Add a Link for adding a new enseignant */}
-    <Link to="/add-enseignant" className="btn btn-primary mb-2">Add Enseignant</Link>
-    <table className="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>No Enseignant</th>
-    <th>Type</th>
-    <th>Sexe</th>
-    <th>Nom</th>
-    <th>Prenom</th>
-    <th>Actions</th>
-    </tr>
-    </thead>
-    <tbody>
-    {
-        enseignants.map((enseignant) => (
-            <tr key={enseignant.NO_ENSEIGNANT}>
-                <td>{enseignant.NO_ENSEIGNANT}</td>
-                <td>{enseignant.TYPE}</td>
-                <td>{enseignant.SEXE}</td>
-                <td>{enseignant.NOM}</td>
-                <td>{enseignant.PRENOM}</td>
-                <td>
-                {/* Add a Link to edit-enseignant */}
-                <Link className="btn btn-info" to={`/edit-enseignant/${enseignant.NO_ENSEIGNANT}`}>Update</Link>
-    <button
-    className="btn btn-danger"
-    onClick={() => deleteEnseignant(enseignant.NO_ENSEIGNANT)}
-    style={{ marginLeft: "10px" }}
->
-    Delete
-    </button>
-    </td>
-    </tr>
-))
-}
-    </tbody>
-    </table>
-    </div>
-);
+            <h2 className="text-center">List Enseignants</h2>
+            {/* Add a Link for adding a new enseignant */}
+            <Link to="/add-enseignant" className="btn btn-primary mb-2">Add Enseignant</Link>
+            <table className="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>No Enseignant</th>
+                    <th>Type</th>
+                    <th>Sexe</th>
+                    <th>Nom</th>
+                    <th>Prenom</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {enseignants.map((enseignant) => (
+                    <tr key={enseignant.NO_ENSEIGNANT}>
+                        <td>{enseignant.NO_ENSEIGNANT}</td>
+                        <td>{enseignant.TYPE}</td>
+                        <td>{enseignant.SEXE}</td>
+                        <td>{enseignant.NOM}</td>
+                        <td>{enseignant.PRENOM}</td>
+                        <td>
+                            {/* Add a Link to edit-enseignant */}
+                            <Link className="btn btn-info" to={`/edit-enseignant/${enseignant.NO_ENSEIGNANT}`}>Update</Link>
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => deleteEnseignant(enseignant.NO_ENSEIGNANT)}
+                                style={{ marginLeft: "10px" }}
+                            >
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default ListEnseignantComponent;
